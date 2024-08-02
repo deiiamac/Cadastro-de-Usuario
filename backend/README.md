59a7552ff4ba7550e8e784f908af126299d83985
devexternoone <dev.externo@one.com.br>
2024-08-02 16:06:35 -0300

    commit

---
+++ backend/README.md
@@ -0,0 +1,12 @@
+## 1° passo
+`cd backend`
+<br><br>
+
+## 2° passo
+Instalar as dependências
+`npm install`
+<br><br>
+
+## 3° passo
+Abrir o projeto
+`npm start`
---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+import React from 'react'
+
+import Logo from '../components/templates/Logo'
+import Nav from '../components/templates/Nav'
+// import Home from '../components/home/Home'
+import Routes from './Routes'
+import Footer from '../components/templates/Footer'
+
+import { BrowserRouter } from 'react-router-dom'
+
+
+export default props =>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
+
